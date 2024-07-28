0088802529d9f4a7edc9c32632b64c67fc60c274
Stella W <shotsinthedark@gmail.com>
2024-07-28 15:47:01 -0400

    Update index.jsx

--- client/src/components/AlbumDisplay/index.jsx
+++ client/src/components/AlbumDisplay/index.jsx
@@ -11,17 +11,23 @@ export default function AlbumDisplay({ albumInfo, editSubmit, isEditing, setIsEd
         </div>
     }
 
+    // isEditing feels like a UI concern - that probably belongs in a lower component than App.jsx - it probably belongs wherever the top of the modal is
+
+    // prop deleteSubmit should probably be called onAlbumDeleteClick 
+
     return (
         <div>
-            {token && <Button onClick={() => setIsEditing(!isEditing)}>Edit</Button>}
+            {token && <Button onClick={() => setIsEditing(!isEditing)}>Edit</Button>} // should probably call "token" "isLoggedIn" or "currentUserToken" or something
             {token && <Button onClick={() => deleteSubmit(albumInfo.id)} >Delete</Button>}
             {!isEditing && <div>
                 <h2>{albumInfo.Album}</h2>
                 Artist: {albumInfo.Artist}<br />
                 Release Year: {albumInfo.Year}<br />
-                Genre Tags: {genreLister(albumInfo.Genre1, albumInfo.Genre2, albumInfo.Genre3)}<br />
+                Genre Tags: {genreLister(albumInfo.Genre1, albumInfo.Genre2, albumInfo.Genre3)}<br /> // Variable names with numbers in them are an indication you should be using an array instead
                 {specialDisplay(albumInfo.Special)}
             </div>}
+
+            // onSubmitClick (or onUploadSubmitClick  ={onEditSubmit}
             {isEditing && <UploadForm albumInfo={albumInfo} handleUploadSubmit={editSubmit} />}
         </div>
             
@@ -37,12 +43,18 @@ function genreLister(genre1, genre2, genre3) {
         genreList = genreList + ", " + genre3;
     }
     return genreList;
+
+    // probably more like genreList=(...genres) {
+    //   return genres.filter(n => n).join(', ');
+    // }
+    // and then this might be simple enough to inline in the html
 }
 
 function specialDisplay(special) {
-    if(special === '*') {
+    if(special === '*') { // arcane symbol - should this a be constant? if this represents "is special" or "is not special" translate this to a boolean somewhere as close to the data layer as possible.
+                          // probably over in server not client
         return "-< SPECIAL ALBUM >-";
-    } else {
+    } else { // unneeded
         return;
     }
 }
