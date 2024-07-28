f393abc71be2fbc295064ca74de0b21569235311
Stella W <shotsinthedark@gmail.com>
2024-07-28 16:02:05 -0400

    Update index.jsx

--- client/src/components/UploadPage/index.jsx
+++ client/src/components/UploadPage/index.jsx
@@ -3,7 +3,9 @@ import UploadForm from "../UploadForm";
 import "./styles.css";
 import { useEffect } from "react";
 
-//COMPONENT that houses all upload pieces
+//COMPONENT that houses all upload pieces   // <--- you dont really need these comments - a file's role should be obvious from how it's named, structured, and where it is in the folder structure
+// Comment where something needs extra explanation
+// And this is often a sign you can restructure/simplify/rename stuff for clarity
 export default function UploadPage({ handleUploadSubmit, token, setToken, uploadQuery, albumToAdd, uploadFetch, setUploadFetch }) {
     const navigate = useNavigate();
 
